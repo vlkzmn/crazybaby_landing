02f9b0c3599bf8ceb680ea0ad61951a1e23be2a8
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-12-22 18:16:45 +0200

    Crazybaby Landing Page

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Crazybaby Landing Page
+
+<p>Presentation landing page for Luna Eye speakers. Provides information about the product, its technical characteristics and advantages.</p>
+<h1><a href="https://vlkzmn.github.io/crazybaby_landing/">LIVE DEMO</a></h1>
+
+# Technologies used
+
+<ul>
+  <li>HTML5</li>
+  <li>CSS3</li>
+  <li>Sass (SCSS)</li>
+  <li>BEM methodology</li>
+  <li>JavaScript</li>
+</ul>
+
+# Features Overview
+
+<p>Layout is adaptive and responsive. It works correctly on all screen sizes (mobile, tablet and desktop). Is implemented using the mobile-first approach.</p>
+<p>The code is valid HTML5 and CSS3. In order to decrease user load and avoid code duplication, styles are implemented using Sass (SCSS) including advanced features like mixins, extends etc. BEM methodology is used for naming classes. This naming convention makes the code more readable and understandable, helps to avoid naming conflicts and makes the code easier to maintain and reuse. The swiper library was used to implement the slider.</p>
+<p>Semantic and meta tags are used. SEO optimization is done. Images are optimized. Accessibility is improved.</p>
+<p><em>see google lighthouse report below (accessibility are not ideal because the lighthouse does not like the light fonts on a white background that the designer intended):</em></p>
+<p align="center">
+  <img src="https://github.com/vlkzmn/crazybaby_landing/raw/master/src/images/lightHouseGoogle.png" width="700px" alt="Museum Landing Page lighthouse">
+</p>
+
+# Preview
+
+<p align="center">
+  <img src="https://github.com/vlkzmn/crazybaby_landing/raw/master/src/images/preview.gif" alt="Museum Landing Page">
+</p>
+
+# In Conclusion
+
+<p>This project aimed to improve my skills in HTML5, CSS3, Sass (SCSS), BEM methodology, JavaScript, responsive and adaptive layout and build the entire landing page positioning elements using all the tools such as flexbox, grid, media queries, etc.</p>
+
+<p>The main challenge was to combine all the knowledge and skills I have acquired so far and implement a landing page from scratch.</p>
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,170 @@
-'use strict';
+/* eslint-disable max-len */
+import Swiper from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/swiper.css';
+import 'swiper/modules/pagination.css';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const pageReload = document.querySelector('#form-reload');
+
+pageReload.addEventListener('submit', function formReload(event) {
+  event.preventDefault();
+  pageReload.reset();
+});
+
+// Slider
+const swiperBenefitsClass = document.querySelector('.benefits__swiper');
+const wrapperBenefitsClass = document.querySelector('.benefits__content');
+const itemsBenefitsClass = document.querySelectorAll('.benefits__item');
+
+const swiperFeaturesClass = document.querySelector('.features__swiper');
+const wrapperFeaturesClass = document.querySelector('.features__swiper-wrapper');
+const itemsFeaturesClass = document.querySelectorAll('.features__content-item');
+
+const menuContent = document.querySelector('.menu__content');
+
+function removeSwiperBenefits() {
+  swiperBenefitsClass.classList.remove('swiper');
+  swiperBenefitsClass.classList.remove('swiper-backface-hidden');
+  wrapperBenefitsClass.classList.remove('swiper-wrapper');
+  wrapperBenefitsClass.removeAttribute('id');
+
+  itemsBenefitsClass.forEach(slide => {
+    slide.classList.remove('swiper-slide');
+    slide.removeAttribute('style');
+  });
+
+  document.querySelector('.benefits__pagination').remove();
+}
+
+function removeSwiperFeatures() {
+  swiperFeaturesClass.classList.remove('swiper');
+  swiperFeaturesClass.classList.remove('swiper-backface-hidden');
+  wrapperFeaturesClass.classList.remove('swiper-wrapper');
+  wrapperFeaturesClass.removeAttribute('id');
+
+  itemsFeaturesClass.forEach(slide => {
+    slide.classList.remove('swiper-slide');
+    slide.removeAttribute('style');
+  });
+}
+
+function addSwiperBenefits() {
+  swiperBenefitsClass.classList.add('swiper');
+  wrapperBenefitsClass.classList.add('swiper-wrapper');
+
+  itemsBenefitsClass.forEach(slide => {
+    slide.classList.add('swiper-slide');
+  });
+
+  const newElement = document.createElement('div');
+
+  newElement.setAttribute('class', 'benefits__pagination swiper-pagination');
+  swiperBenefitsClass.appendChild(newElement);
+}
+
+function addSwiperFeatures() {
+  swiperFeaturesClass.classList.add('swiper');
+  wrapperFeaturesClass.classList.add('swiper-wrapper');
+
+  itemsFeaturesClass.forEach(slide => {
+    slide.classList.add('swiper-slide');
+  });
+}
+
+function initSwiperBenefits() {
+  return new Swiper('.benefits__swiper', {
+    modules: [Pagination],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+
+function initSwiperFeatures() {
+  return new Swiper('.features__swiper', {
+    modules: [Pagination, Navigation],
+    pagination: {
+      el: '.features__slider-count',
+      type: 'fraction',
+      renderFraction: function(current, total) {
+        return `
+          0<span class="${current}"></span>
+          <span class="features__slider-count-down">/ 0</span>
+          <span class="features__slider-count-down ${total}"></span>
+        `;
+      },
+    },
+    navigation: {
+      nextEl: '.features__icon--right',
+      prevEl: '.features__icon--left',
+    },
+  });
+}
+
+let swiperBenefits = null;
+let swiperFeatures = null;
+
+if (window.innerWidth < 768) {
+  addSwiperBenefits();
+  addSwiperFeatures();
+  swiperBenefits = initSwiperBenefits();
+  swiperFeatures = initSwiperFeatures();
+}
+
+if (window.innerWidth < 1194) {
+  addSwiperFeatures();
+  swiperFeatures = initSwiperFeatures();
+}
+
+function checkScreenWidth() {
+  if (window.innerWidth >= 1194) {
+    if (swiperBenefits) {
+      swiperBenefits.destroy(true, true);
+      swiperBenefits = null;
+      removeSwiperBenefits();
+    }
+
+    if (swiperFeatures) {
+      swiperFeatures.destroy(true, true);
+      swiperFeatures = null;
+      removeSwiperFeatures();
+    }
+  } else if (window.innerWidth >= 768) {
+    if (swiperBenefits) {
+      swiperBenefits.destroy(true, true);
+      swiperBenefits = null;
+      removeSwiperBenefits();
+    }
+
+    if (!swiperFeatures) {
+      addSwiperFeatures();
+      swiperFeatures = initSwiperFeatures();
+    }
+
+    menuContent.style.height = '100vh';
+  } else {
+    if (!swiperBenefits) {
+      addSwiperBenefits();
+      swiperBenefits = initSwiperBenefits();
+    }
+
+    if (!swiperFeatures) {
+      addSwiperFeatures();
+      swiperFeatures = initSwiperFeatures();
+    }
+
+    menuContent.style.height = document.documentElement.clientHeight + 'px';
+  }
+}
+
+checkScreenWidth();
+window.addEventListener('resize', checkScreenWidth);
